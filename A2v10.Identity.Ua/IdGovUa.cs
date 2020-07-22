049592ca56d72c11c01bae0f9ddd83e47c348026
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-07-22 07:29:15 +0300

    Add project files.

---
+++ A2v10.Identity.Ua/IdGovUa.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2v10.Identity.Ua
+{
+	public class IdGovUa : Id
+	{
+		public IdGovUa(IDbContext dbContext)
+		{
+
+		}
+		
+		public ActionResult Start()
+	}
+}
